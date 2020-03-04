9c883c73f889bf3627b7fc6d8df98ab4d458d13c
Helen Du <59598171+helen2180@users.noreply.github.com>
2020-03-04 00:04:16 -0800

    updated main with nokia stuff

--- main.c
+++ main.c
@@ -1,78 +1,120 @@
 #include <avr/io.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <util/delay.h>
+
+#include "nokia5110.h"
 #include "io.h"
 #include "joystick.h"
 
-#define button (~PINA & 0x04)
-#define reset (~PINA & 0x08)
+unsigned char joy;
+enum state {idle, up, down, left, right} state;
 
-// button is also the dig button
-
-enum state {wait, press} state;
-
-unsigned char x;
-unsigned char i;
-unsigned char temp = 0x00;
-
-void tick() {
-	switch (state) { //transitions
-		case wait:
-			if (button) {
-				state = press;
-				for (i = 0; i < 4; i++) {
-					x = rand() % 2;
-					temp = temp | (x << i);
-				}
-			}
-			else {
-				state = wait;
-			}
-			break;
-		case press:
-			if (button) {
-				state = press;
-			}
-			else {
-				state = wait;
-				temp = 0;
-			}
-			break;
+void tick_js() {
+	joy = js();
+	switch (state) {
+		case idle:
+		if (joy == 1) {
+			LCD_ClearScreen();
+			LCD_DisplayString(1, "Up");
+			nokia_lcd_set_cursor(22, 0);
+			nokia_lcd_write_string("Weather", 1);
+			nokia_lcd_set_cursor(15, 12);
+			nokia_lcd_write_string("Sunny", 2);
+			nokia_lcd_set_cursor(0, 30);
+			nokia_lcd_write_string("Enjoy a warm  and sunny day!", 1);
+			nokia_lcd_render();
+			state = up;
+		}
+		else if (joy == 2) {
+			LCD_ClearScreen();
+			LCD_DisplayString(1, "Down");
+			nokia_lcd_set_cursor(22, 0);
+			nokia_lcd_write_string("Weather", 1);
+			nokia_lcd_set_cursor(15, 12);
+			nokia_lcd_write_string("Rainy", 2);
+			nokia_lcd_set_cursor(10, 30);
+			nokia_lcd_write_string("Remember to", 1);
+			nokia_lcd_set_cursor(15, 38);
+			nokia_lcd_write_string("stay dry!", 1);
+			nokia_lcd_render();
+			state = down;
+		}
+		else if (joy == 3) {
+			LCD_ClearScreen();
+			LCD_DisplayString(1, "Left");
+			nokia_lcd_set_cursor(22, 0);
+			nokia_lcd_write_string("Weather", 1);
+			nokia_lcd_set_cursor(10, 12);
+			nokia_lcd_write_string("Cloudy", 2);
+			nokia_lcd_set_cursor(10, 30);
+			nokia_lcd_write_string("Remember to", 1);
+			nokia_lcd_set_cursor(5, 38);
+			nokia_lcd_write_string("dress warmly!", 1);
+			nokia_lcd_render();
+			state = left;
+		}
+		else if (joy == 4) {
+			LCD_ClearScreen();
+			LCD_DisplayString(1, "Right");
+			state = right;
+		}
+		else {
+			state = idle;
+		}
+		break;
+		case up:
+		if (joy == 1) {
+			state = up;
+		}
+		else {
+			state = idle;
+			LCD_ClearScreen();
+			nokia_lcd_clear();
+		}
+		break;
+		case down:
+		if (joy == 2) {
+			state = down;
+		}
+		else {
+			state = idle;
+			LCD_ClearScreen();
+			nokia_lcd_clear();
+		}
+		break;
+		case left:
+		if (joy == 3) {
+			state = left;
+		}
+		else {
+			state = idle;
+			LCD_ClearScreen();
+			nokia_lcd_clear();
+		}
+		break;
+		case right:
+		if (joy == 4) {
+			state = right;
+		}
+		else {
+			state = idle;
+			LCD_ClearScreen();
+		}
+		break;
 	}
 }
 
 int main(void) {
-  DDRA = 0x00; PORTA = 0xFF;
-	DDRB = 0xFF; PORTB = 0x00;
+	DDRA = 0x00; PORTA = 0xFF;
 	DDRC = 0xFF; PORTC = 0x00;
 	DDRD = 0xFF; PORTD = 0x00;
 
-  srand(time(0));
-
-	unsigned char joy;
-
 	ADC_init();
 	LCD_init();
-
-  while (1) {
-    tick();
-	  joy = js();
-    if (joy == 1) {
-      LCD_ClearScreen();
-    	LCD_DisplayString(1, "Up");
-    }
-    else if (joy == 2) {
-  		LCD_ClearScreen();
-    	LCD_DisplayString(1, "Down");
-    }
-  	else if (joy == 3) {
-    	LCD_ClearScreen();
-    	LCD_DisplayString(1, "Left");
-    }
-    else if (joy == 4) {
-    	LCD_ClearScreen();
-    	LCD_DisplayString(1, "Right");
-    }
-  }
+	
+	nokia_lcd_init();
+	nokia_lcd_clear();
+	
+	while (1) {
+		tick_js();
+	}
 }
