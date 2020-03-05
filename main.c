992b0d59437dc051b471f1ec8110fbf8bdf757a2
Helen Du <59598171+helen2180@users.noreply.github.com>
2020-03-05 00:45:46 -0800

    added nokia screen functions

--- main.c
+++ main.c
@@ -5,101 +5,204 @@
 #include "io.h"
 #include "joystick.h"
 
+// Nokia 5110 screen functions
+void title_screen() {
+	nokia_lcd_clear();
+	nokia_lcd_set_cursor(20, 0);
+	nokia_lcd_write_string("Just", 2);
+	nokia_lcd_set_cursor(15, 15);
+	nokia_lcd_write_string("Dance", 2);
+	nokia_lcd_set_cursor(12, 35);
+	nokia_lcd_write_string("Revolution", 1);
+	nokia_lcd_render();
+}
+		
+void demo_init() {
+	nokia_lcd_clear();
+	nokia_lcd_set_cursor(1, 5);
+	nokia_lcd_write_string("Remember this:", 1);
+	nokia_lcd_render();
+}
+		
+void demo_up() {
+	nokia_lcd_clear();
+	nokia_lcd_set_cursor(1, 5);
+	nokia_lcd_write_string("Remember this:", 1);
+	nokia_lcd_set_cursor(28, 20);
+	nokia_lcd_write_string("Up", 3);
+	nokia_lcd_render();
+}
+		
+void demo_down() {
+	nokia_lcd_clear();
+	nokia_lcd_set_cursor(1, 5);
+	nokia_lcd_write_string("Remember this:", 1);
+	nokia_lcd_set_cursor(10, 20);
+	nokia_lcd_write_string("Down", 3);
+	nokia_lcd_render();
+}
+		
+void demo_left() {
+	nokia_lcd_clear();
+	nokia_lcd_set_cursor(1, 5);
+	nokia_lcd_write_string("Remember this:", 1);
+	nokia_lcd_set_cursor(10, 20);
+	nokia_lcd_write_string("Left", 3);
+	nokia_lcd_render();
+}
+		
+void demo_right() {
+	nokia_lcd_clear();
+	nokia_lcd_set_cursor(1, 5);
+	nokia_lcd_write_string("Remember this:", 1);
+	nokia_lcd_set_cursor(2, 20);
+	nokia_lcd_write_string("Right", 3);
+	nokia_lcd_render();
+}
+
+void play_init() {
+	nokia_lcd_clear();
+	nokia_lcd_set_cursor(13, 5);
+	nokia_lcd_write_string("Your Turn:", 1);
+	nokia_lcd_render();
+}
+
+void play_up() {
+	nokia_lcd_clear();
+	nokia_lcd_set_cursor(13, 5);
+	nokia_lcd_write_string("Your Turn:", 1);
+	nokia_lcd_set_cursor(28, 20);
+	nokia_lcd_write_string("Up", 3);
+	nokia_lcd_render();
+}
+		
+void play_down() {
+	nokia_lcd_clear();
+	nokia_lcd_set_cursor(13, 5);
+	nokia_lcd_write_string("Your Turn:", 1);
+	nokia_lcd_set_cursor(10, 20);
+	nokia_lcd_write_string("Down", 3);
+	nokia_lcd_render();
+}
+		
+void play_left() {
+	nokia_lcd_clear();
+	nokia_lcd_set_cursor(13, 5);
+	nokia_lcd_write_string("Your Turn:", 1);
+	nokia_lcd_set_cursor(10, 20);
+	nokia_lcd_write_string("Left", 3);
+	nokia_lcd_render();
+}
+		
+void play_right() {
+	nokia_lcd_clear();
+	nokia_lcd_set_cursor(13, 5);
+	nokia_lcd_write_string("Your Turn:", 1);
+	nokia_lcd_set_cursor(2, 20);
+	nokia_lcd_write_string("Right", 3);
+	nokia_lcd_render();
+}
+		
+void game_over_screen () {
+	nokia_lcd_clear();
+	nokia_lcd_set_cursor(20, 5);
+	nokia_lcd_write_string("Game", 2);
+	nokia_lcd_set_cursor(20, 25);
+	nokia_lcd_write_string("Over", 2);
+	nokia_lcd_render();
+}
+		
+void win_screen () {
+	nokia_lcd_clear();
+	nokia_lcd_set_cursor(27, 5);
+	nokia_lcd_write_string("You", 2);
+	nokia_lcd_set_cursor(24, 25);
+	nokia_lcd_write_string("Win!", 2);
+	nokia_lcd_render();
+}
+
+void play_again_screen() {
+	nokia_lcd_clear();
+	nokia_lcd_set_cursor(20, 5);
+	nokia_lcd_write_string("Play", 2);
+	nokia_lcd_set_cursor(10, 25);
+	nokia_lcd_write_string("Again?", 2);
+	nokia_lcd_render();
+}
+
+#define button (~PINA & 0x04)
 unsigned char joy;
-enum state {idle, up, down, left, right} state;
+enum joy_state {start, idle, up, down, left, right} joy_state;
 
 void tick_js() {
 	joy = js();
-	switch (state) {
+	switch (joy_state) {
+		case start:
+			if (button) {
+				play_init();
+				joy_state = idle;
+			}
+			else {
+				joy_state = start;
+			}
+			break;
 		case idle:
-		if (joy == 1) {
-			LCD_ClearScreen();
-			LCD_DisplayString(1, "Up");
-			nokia_lcd_set_cursor(22, 0);
-			nokia_lcd_write_string("Weather", 1);
-			nokia_lcd_set_cursor(15, 12);
-			nokia_lcd_write_string("Sunny", 2);
-			nokia_lcd_set_cursor(0, 30);
-			nokia_lcd_write_string("Enjoy a warm  and sunny day!", 1);
-			nokia_lcd_render();
-			state = up;
-		}
-		else if (joy == 2) {
-			LCD_ClearScreen();
-			LCD_DisplayString(1, "Down");
-			nokia_lcd_set_cursor(22, 0);
-			nokia_lcd_write_string("Weather", 1);
-			nokia_lcd_set_cursor(15, 12);
-			nokia_lcd_write_string("Rainy", 2);
-			nokia_lcd_set_cursor(10, 30);
-			nokia_lcd_write_string("Remember to", 1);
-			nokia_lcd_set_cursor(15, 38);
-			nokia_lcd_write_string("stay dry!", 1);
-			nokia_lcd_render();
-			state = down;
-		}
-		else if (joy == 3) {
-			LCD_ClearScreen();
-			LCD_DisplayString(1, "Left");
-			nokia_lcd_set_cursor(22, 0);
-			nokia_lcd_write_string("Weather", 1);
-			nokia_lcd_set_cursor(10, 12);
-			nokia_lcd_write_string("Cloudy", 2);
-			nokia_lcd_set_cursor(10, 30);
-			nokia_lcd_write_string("Remember to", 1);
-			nokia_lcd_set_cursor(5, 38);
-			nokia_lcd_write_string("dress warmly!", 1);
-			nokia_lcd_render();
-			state = left;
-		}
-		else if (joy == 4) {
-			LCD_ClearScreen();
-			LCD_DisplayString(1, "Right");
-			state = right;
-		}
-		else {
-			state = idle;
-		}
-		break;
+			if (joy == 1) {
+				joy_state = up;
+				play_up();
+			}
+			else if (joy == 2) {
+				joy_state = down;
+				play_down();
+			}
+			else if (joy == 3) {
+				joy_state = left;
+				play_left();
+			}
+			else if (joy == 4) {
+				joy_state = right;
+				play_right();
+			}
+			else {
+				joy_state = idle;
+			}
+			break;
 		case up:
-		if (joy == 1) {
-			state = up;
-		}
-		else {
-			state = idle;
-			LCD_ClearScreen();
-			nokia_lcd_clear();
-		}
-		break;
+			if (joy == 1) {
+				joy_state = up;
+			}
+			else {
+				joy_state = idle;
+				play_init();
+			}
+			break;
 		case down:
-		if (joy == 2) {
-			state = down;
-		}
-		else {
-			state = idle;
-			LCD_ClearScreen();
-			nokia_lcd_clear();
-		}
-		break;
+			if (joy == 2) {
+				joy_state = down;
+			}
+			else {
+				joy_state = idle;
+				play_init();
+			}
+			break;
 		case left:
-		if (joy == 3) {
-			state = left;
-		}
-		else {
-			state = idle;
-			LCD_ClearScreen();
-			nokia_lcd_clear();
-		}
-		break;
+			if (joy == 3) {
+				joy_state = left;
+			}
+			else {
+				joy_state = idle;
+				play_init();
+			}
+			break;
 		case right:
-		if (joy == 4) {
-			state = right;
-		}
-		else {
-			state = idle;
-			LCD_ClearScreen();
-		}
-		break;
+			if (joy == 4) {
+				joy_state = right;
+			}
+			else {
+				joy_state = idle;
+				play_init();
+			}
+			break;
 	}
 }
 
@@ -110,7 +213,6 @@ int main(void) {
 
 	ADC_init();
 	LCD_init();
-	
 	nokia_lcd_init();
 	nokia_lcd_clear();
 	
