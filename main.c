46000069f999b72720d68ff6d16699af63cab607
Helen Du <59598171+helen2180@users.noreply.github.com>
2020-03-05 01:21:45 -0800

    cleaned up code

--- main.c
+++ main.c
@@ -1,137 +1,17 @@
 #include <avr/io.h>
 #include <util/delay.h>
-
 #include "nokia5110.h"
 #include "io.h"
 #include "joystick.h"
-
-// Nokia 5110 screen functions
-void title_screen() {
-	nokia_lcd_clear();
-	nokia_lcd_set_cursor(20, 0);
-	nokia_lcd_write_string("Just", 2);
-	nokia_lcd_set_cursor(15, 15);
-	nokia_lcd_write_string("Dance", 2);
-	nokia_lcd_set_cursor(12, 35);
-	nokia_lcd_write_string("Revolution", 1);
-	nokia_lcd_render();
-}
-		
-void demo_init() {
-	nokia_lcd_clear();
-	nokia_lcd_set_cursor(1, 5);
-	nokia_lcd_write_string("Remember this:", 1);
-	nokia_lcd_render();
-}
-		
-void demo_up() {
-	nokia_lcd_clear();
-	nokia_lcd_set_cursor(1, 5);
-	nokia_lcd_write_string("Remember this:", 1);
-	nokia_lcd_set_cursor(28, 20);
-	nokia_lcd_write_string("Up", 3);
-	nokia_lcd_render();
-}
-		
-void demo_down() {
-	nokia_lcd_clear();
-	nokia_lcd_set_cursor(1, 5);
-	nokia_lcd_write_string("Remember this:", 1);
-	nokia_lcd_set_cursor(10, 20);
-	nokia_lcd_write_string("Down", 3);
-	nokia_lcd_render();
-}
-		
-void demo_left() {
-	nokia_lcd_clear();
-	nokia_lcd_set_cursor(1, 5);
-	nokia_lcd_write_string("Remember this:", 1);
-	nokia_lcd_set_cursor(10, 20);
-	nokia_lcd_write_string("Left", 3);
-	nokia_lcd_render();
-}
-		
-void demo_right() {
-	nokia_lcd_clear();
-	nokia_lcd_set_cursor(1, 5);
-	nokia_lcd_write_string("Remember this:", 1);
-	nokia_lcd_set_cursor(2, 20);
-	nokia_lcd_write_string("Right", 3);
-	nokia_lcd_render();
-}
-
-void play_init() {
-	nokia_lcd_clear();
-	nokia_lcd_set_cursor(13, 5);
-	nokia_lcd_write_string("Your Turn:", 1);
-	nokia_lcd_render();
-}
-
-void play_up() {
-	nokia_lcd_clear();
-	nokia_lcd_set_cursor(13, 5);
-	nokia_lcd_write_string("Your Turn:", 1);
-	nokia_lcd_set_cursor(28, 20);
-	nokia_lcd_write_string("Up", 3);
-	nokia_lcd_render();
-}
-		
-void play_down() {
-	nokia_lcd_clear();
-	nokia_lcd_set_cursor(13, 5);
-	nokia_lcd_write_string("Your Turn:", 1);
-	nokia_lcd_set_cursor(10, 20);
-	nokia_lcd_write_string("Down", 3);
-	nokia_lcd_render();
-}
-		
-void play_left() {
-	nokia_lcd_clear();
-	nokia_lcd_set_cursor(13, 5);
-	nokia_lcd_write_string("Your Turn:", 1);
-	nokia_lcd_set_cursor(10, 20);
-	nokia_lcd_write_string("Left", 3);
-	nokia_lcd_render();
-}
-		
-void play_right() {
-	nokia_lcd_clear();
-	nokia_lcd_set_cursor(13, 5);
-	nokia_lcd_write_string("Your Turn:", 1);
-	nokia_lcd_set_cursor(2, 20);
-	nokia_lcd_write_string("Right", 3);
-	nokia_lcd_render();
-}
-		
-void game_over_screen () {
-	nokia_lcd_clear();
-	nokia_lcd_set_cursor(20, 5);
-	nokia_lcd_write_string("Game", 2);
-	nokia_lcd_set_cursor(20, 25);
-	nokia_lcd_write_string("Over", 2);
-	nokia_lcd_render();
-}
-		
-void win_screen () {
-	nokia_lcd_clear();
-	nokia_lcd_set_cursor(27, 5);
-	nokia_lcd_write_string("You", 2);
-	nokia_lcd_set_cursor(24, 25);
-	nokia_lcd_write_string("Win!", 2);
-	nokia_lcd_render();
-}
-
-void play_again_screen() {
-	nokia_lcd_clear();
-	nokia_lcd_set_cursor(20, 5);
-	nokia_lcd_write_string("Play", 2);
-	nokia_lcd_set_cursor(10, 25);
-	nokia_lcd_write_string("Again?", 2);
-	nokia_lcd_render();
-}
+#include "scheduler.h"
+#include "timer.h"
+#include "bit.h"
+#include "screens.h"
+#include "PWM.h"
 
 #define button (~PINA & 0x04)
 unsigned char joy;
+
 enum joy_state {start, idle, up, down, left, right} joy_state;
 
 void tick_js() {
@@ -140,6 +20,7 @@ void tick_js() {
 		case start:
 			if (button) {
 				play_init();
+				PWM_on();
 				joy_state = idle;
 			}
 			else {
@@ -149,18 +30,22 @@ void tick_js() {
 		case idle:
 			if (joy == 1) {
 				joy_state = up;
+				set_PWM(F4);
 				play_up();
 			}
 			else if (joy == 2) {
 				joy_state = down;
+				set_PWM(C4);
 				play_down();
 			}
 			else if (joy == 3) {
 				joy_state = left;
+				set_PWM(D4);
 				play_left();
 			}
 			else if (joy == 4) {
 				joy_state = right;
+				set_PWM(E4);
 				play_right();
 			}
 			else {
@@ -174,6 +59,7 @@ void tick_js() {
 			else {
 				joy_state = idle;
 				play_init();
+				set_PWM(0);
 			}
 			break;
 		case down:
@@ -183,6 +69,7 @@ void tick_js() {
 			else {
 				joy_state = idle;
 				play_init();
+				set_PWM(0);
 			}
 			break;
 		case left:
@@ -192,6 +79,7 @@ void tick_js() {
 			else {
 				joy_state = idle;
 				play_init();
+				set_PWM(0);
 			}
 			break;
 		case right:
@@ -201,6 +89,7 @@ void tick_js() {
 			else {
 				joy_state = idle;
 				play_init();
+				set_PWM(0);
 			}
 			break;
 	}
@@ -208,6 +97,7 @@ void tick_js() {
 
 int main(void) {
 	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0xFF; PORTB = 0x00;
 	DDRC = 0xFF; PORTC = 0x00;
 	DDRD = 0xFF; PORTD = 0x00;
 
@@ -215,6 +105,8 @@ int main(void) {
 	LCD_init();
 	nokia_lcd_init();
 	nokia_lcd_clear();
+	title_screen();
+	LCD_DisplayString(1, "Click the Button to begin!");
 	
 	while (1) {
 		tick_js();
