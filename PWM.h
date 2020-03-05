49e52e51e969c500d5fb85215050c1cac6013af2
Helen Du <59598171+helen2180@users.noreply.github.com>
2020-03-05 01:21:32 -0800

    added more libraries

---
+++ PWM.h
@@ -0,0 +1,38 @@
+#ifndef PWM_H_
+#define PWM_H_
+
+#define C4 (261.63)
+#define D4 (293.66)
+#define E4 (329.63)
+#define F4 (349.23)
+
+void set_PWM(double frequency);
+void PWM_on();
+void PWM_off();
+
+void set_PWM(double frequency) {
+	static double current_frequency;
+	if (frequency != current_frequency) {
+		if (!frequency) { TCCR3B &= 0x08; }
+		else { TCCR3B |= 0x03; }
+		if (frequency < 0.954) { OCR3A = 0xFFFF; }
+		else if (frequency > 31250) { OCR3A = 0x0000; }
+		else { OCR3A = (short) (8000000 / (128 * frequency)) - 1; }
+		TCNT3 = 0;
+		current_frequency = frequency;
+	}
+}
+
+void PWM_on() {
+	TCCR3A = (1 << COM3A0);
+	TCCR3B = (1 << WGM32) | (1 << CS31) | (1 << CS30);
+	set_PWM(0);
+}
+
+void PWM_off() {
+	TCCR3A = 0x00;
+	TCCR3B = 0x00;
+}
+
+
+#endif /* PWM_H_ */
---
+++ screens.h
@@ -0,0 +1,147 @@
+#ifndef SCREENS_H_
+#define SCREENS_H_
+
+// Nokia 5110 screen functions
+void title_screen();
+void game_over_screen ();
+void win_screen ();
+void play_again_screen();
+
+void demo_init();
+void demo_up();
+void demo_down();
+void demo_left();
+void demo_right();
+
+void play_init();
+void play_up();
+void play_down();
+void play_left();
+void play_right();
+
+
+void title_screen() {
+	nokia_lcd_clear();
+	nokia_lcd_set_cursor(20, 0);
+	nokia_lcd_write_string("Just", 2);
+	nokia_lcd_set_cursor(15, 15);
+	nokia_lcd_write_string("Dance", 2);
+	nokia_lcd_set_cursor(12, 35);
+	nokia_lcd_write_string("Revolution", 1);
+	nokia_lcd_render();
+}
+
+void demo_init() {
+	nokia_lcd_clear();
+	nokia_lcd_set_cursor(1, 5);
+	nokia_lcd_write_string("Remember this:", 1);
+	nokia_lcd_render();
+}
+
+void demo_up() {
+	nokia_lcd_clear();
+	nokia_lcd_set_cursor(1, 5);
+	nokia_lcd_write_string("Remember this:", 1);
+	nokia_lcd_set_cursor(28, 20);
+	nokia_lcd_write_string("Up", 3);
+	nokia_lcd_render();
+}
+
+void demo_down() {
+	nokia_lcd_clear();
+	nokia_lcd_set_cursor(1, 5);
+	nokia_lcd_write_string("Remember this:", 1);
+	nokia_lcd_set_cursor(10, 20);
+	nokia_lcd_write_string("Down", 3);
+	nokia_lcd_render();
+}
+
+void demo_left() {
+	nokia_lcd_clear();
+	nokia_lcd_set_cursor(1, 5);
+	nokia_lcd_write_string("Remember this:", 1);
+	nokia_lcd_set_cursor(10, 20);
+	nokia_lcd_write_string("Left", 3);
+	nokia_lcd_render();
+}
+
+void demo_right() {
+	nokia_lcd_clear();
+	nokia_lcd_set_cursor(1, 5);
+	nokia_lcd_write_string("Remember this:", 1);
+	nokia_lcd_set_cursor(2, 20);
+	nokia_lcd_write_string("Right", 3);
+	nokia_lcd_render();
+}
+
+void play_init() {
+	nokia_lcd_clear();
+	nokia_lcd_set_cursor(13, 5);
+	nokia_lcd_write_string("Your Turn:", 1);
+	nokia_lcd_render();
+}
+
+void play_up() {
+	nokia_lcd_clear();
+	nokia_lcd_set_cursor(13, 5);
+	nokia_lcd_write_string("Your Turn:", 1);
+	nokia_lcd_set_cursor(28, 20);
+	nokia_lcd_write_string("Up", 3);
+	nokia_lcd_render();
+}
+
+void play_down() {
+	nokia_lcd_clear();
+	nokia_lcd_set_cursor(13, 5);
+	nokia_lcd_write_string("Your Turn:", 1);
+	nokia_lcd_set_cursor(10, 20);
+	nokia_lcd_write_string("Down", 3);
+	nokia_lcd_render();
+}
+
+void play_left() {
+	nokia_lcd_clear();
+	nokia_lcd_set_cursor(13, 5);
+	nokia_lcd_write_string("Your Turn:", 1);
+	nokia_lcd_set_cursor(10, 20);
+	nokia_lcd_write_string("Left", 3);
+	nokia_lcd_render();
+}
+
+void play_right() {
+	nokia_lcd_clear();
+	nokia_lcd_set_cursor(13, 5);
+	nokia_lcd_write_string("Your Turn:", 1);
+	nokia_lcd_set_cursor(2, 20);
+	nokia_lcd_write_string("Right", 3);
+	nokia_lcd_render();
+}
+
+void game_over_screen () {
+	nokia_lcd_clear();
+	nokia_lcd_set_cursor(20, 5);
+	nokia_lcd_write_string("Game", 2);
+	nokia_lcd_set_cursor(20, 25);
+	nokia_lcd_write_string("Over", 2);
+	nokia_lcd_render();
+}
+
+void win_screen () {
+	nokia_lcd_clear();
+	nokia_lcd_set_cursor(27, 5);
+	nokia_lcd_write_string("You", 2);
+	nokia_lcd_set_cursor(24, 25);
+	nokia_lcd_write_string("Win!", 2);
+	nokia_lcd_render();
+}
+
+void play_again_screen() {
+	nokia_lcd_clear();
+	nokia_lcd_set_cursor(20, 5);
+	nokia_lcd_write_string("Play", 2);
+	nokia_lcd_set_cursor(10, 25);
+	nokia_lcd_write_string("Again?", 2);
+	nokia_lcd_render();
+}
+
+#endif /* SCREENS_H_ */
