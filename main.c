030cd0616f8bffb31a63dfe8238f92f26289949b
Helen Du <59598171+helen2180@users.noreply.github.com>
2020-03-05 12:41:02 -0800

    attempting to write state machines

--- main.c
+++ main.c
@@ -9,57 +9,109 @@
 #include "PWM.h"
 
 #define button (~PINA & 0x04)
+unsigned char notes[8] = {1, 2, 3, 4, 1, 2, 3, 4,};
+unsigned char game_begin;
+unsigned char demo_begin;
+unsigned char player_begin;
 unsigned char joy;
+unsigned char i;
 
-enum joy_state {start, idle, up, down, left, right} joy_state;
+enum init_state {init1, wait1, start}; init_state;
+enum demo_state {init2, wait2, play, blank}; demo_state;
+enum play_state {init3, idle, up, down, left, right} play_state;
 
-void tick_js() {
-	joy = js();
-	switch (joy_state) {
-		case start:
+void tick_init() {
+	switch (init_state) {
+		case init1:
+			game_begin = 0;
+			player_begin = 0;
+			demo_begin = 0;
 			title_screen();
 			PORTC = 0xFF;
+			init_state = wait1;
+			break;
+		case wait1: // add function that creates a random array of 8 numbers between 1-4
 			if (button) {
-				play_init();
-				PWM_on();
-				joy_state = idle;
+				init_state = start;
+				game_begin = 1;
+				demo_begin = 1;
+				break;
 			}
 			else {
-				joy_state = start;
+				init_state = wait1;
+				game_begin = 0;
+			}
+			break;
+		case start:
+			break;
+	}
+}
+
+void tick_demo () {
+	switch(demo_state) {
+		case init2:
+			if (game_begin && demo_begin) {
+				demo_state = wait2;
+			}
+			else {
+				demo_state = init2;
+			}
+			break;
+		case wait2:
+			demo_init();
+			break;
+		case play:
+			break;
+		case blank:
+			break;
+	}
+}
+
+void tick_player() {
+	joy = js();
+	switch (play_state) {
+		case init2:
+			if (game_begin && player_begin) {
+				play_init();
+				PWM_on();
+				play_state = idle;
+			}
+			else {
+				play_state = init2;
 			}
 			break;
 		case idle:
 			if (joy == 1) {
-				joy_state = up;
+				play_state = up;
 				set_PWM(F4);
 				play_up();
 				PORTC = 0x00;
 			}
 			else if (joy == 2) {
-				joy_state = down;
+				play_state = down;
 				set_PWM(C4);
 				play_down();
 			}
 			else if (joy == 3) {
-				joy_state = left;
+				play_state = left;
 				set_PWM(D4);
 				play_left();
 			}
 			else if (joy == 4) {
-				joy_state = right;
+				play_state = right;
 				set_PWM(E4);
 				play_right();
 			}
 			else {
-				joy_state = idle;
+				play_state = idle;
 			}
 			break;
 		case up:
 			if (joy == 1) {
-				joy_state = up;
+				play_state = up;
 			}
 			else {
-				joy_state = idle;
+				play_state = idle;
 				play_init();
 				set_PWM(0);
 				PORTC = 0xFF;
@@ -67,30 +119,30 @@ void tick_js() {
 			break;
 		case down:
 			if (joy == 2) {
-				joy_state = down;
+				play_state = down;
 			}
 			else {
-				joy_state = idle;
+				play_state = idle;
 				play_init();
 				set_PWM(0);
 			}
 			break;
 		case left:
 			if (joy == 3) {
-				joy_state = left;
+				play_state = left;
 			}
 			else {
-				joy_state = idle;
+				play_state = idle;
 				play_init();
 				set_PWM(0);
 			}
 			break;
 		case right:
 			if (joy == 4) {
-				joy_state = right;
+				play_state = right;
 			}
 			else {
-				joy_state = idle;
+				play_state = idle;
 				play_init();
 				set_PWM(0);
 			}
@@ -102,19 +154,30 @@ int main(void) {
 	DDRA = 0x00; PORTA = 0xFF;
 	DDRB = 0xFF; PORTB = 0x00;
 	DDRC = 0xFF; PORTC = 0x00;
-
+	
 	ADC_init();
 	nokia_lcd_init();
 	nokia_lcd_clear();
+	game_begin = 0;
 	
-	static task task1;
-	task *tasks[] = { &task1};
+	static task task1, task2, task3;
+	task *tasks[] = { &task1, &task2, &task3};
 	const unsigned short numTasks = sizeof(tasks)/sizeof(task*);
 	
 	task1.state = 0;
 	task1.period = 50;
 	task1.elapsedTime = task1.period;
-	task1.TickFct = &tick_js;
+	task1.TickFct = &tick_init;
+	
+	task2.state = 0;
+	task2.period = 700;
+	task2.elapsedTime = task2.period;
+	task2.TickFct = &tick_demo;
+	
+	task3.state = 0;
+	task3.period = 50;
+	task3.elapsedTime = task3.period;
+	task3.TickFct = &tick_player;
 	
 	TimerSet(1);
 	TimerOn();
